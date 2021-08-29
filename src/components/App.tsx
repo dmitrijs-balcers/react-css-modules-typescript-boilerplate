9a8046ad3fcb83a9ecbf22b1a3dbf3deebc7d3ec
Dmitrijs Balcers <dbalcers@evolutiongaming.com>
2021-08-29 15:37:00 +0300

    Add prettier and husky

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,10 @@ import { withBorder, background } from "./App.module.css";
 export const App: React.FC = () => {
     const [number, increase] = React.useState<number>(0);
 
-    return <>
-        <div className={cn(withBorder, background)}>It Works! {number}</div>
-        <button onClick={() => increase(number + 1)}>Click!</button>
-    </>;
+    return (
+        <>
+            <div className={cn(withBorder, background)}>It Works! {number}</div>
+            <button onClick={() => increase(number + 1)}>Click!</button>
+        </>
+    );
 };
